05cd6ec598227912c7564a9e8789f01479bf3ff9
JasonRolle1990 <89821605+JasonRolle1990@users.noreply.github.com>
2021-12-14 17:51:23 -0500

    changes

--- open/open/settings.py
+++ open/open/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'open.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'media')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
